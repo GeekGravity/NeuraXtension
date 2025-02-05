128ed2fa4a21e4ab5857f3845f13ceb02c7d7f9d
GeekGravity <mathewsjais20@gmail.com>
2025-02-05 11:00:58 -0800

    Made About Us responsive

--- src/Pages/AboutUs.tsx
+++ src/Pages/AboutUs.tsx
@@ -5,64 +5,42 @@ import Team from "./Team";
 function AboutUs() {
   return (
     <>
-      <div className=" MissionBox">
+      <section className="row align-items-center MissionBox py-4">
+        <div className="col-md-6 col-12 my-4 py-md-5">
+          <img className="img-fluid d-block mx-auto" src={aboutus} />
+        </div>
+        <div className="col-md-6 col-12 text-center my-2 py-md-5 my-lg-0">
+          <h1 className="h2">OUR MISSION</h1>
+          <p className="my-4 mx-lg-5 px-4">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, iste
+            molestiae beatae, maiores deserunt in voluptatibus aspernatur
+            architecto excepturi delectus soluta? Ipsa, deleniti dolorem hic
+            consequatur repellat eveniet quidem voluptate necessitatibus dolorum
+            delectus minus vitae, ut, veritatis sint ipsum magnam autem nam ex
+            deserunt debitis eaque ratione! Nobis, quidem assumenda.Lorem ipsum
+            dolor sit amet consectetur adipisicing elit. Neque, iste molestiae
+            beatae, maiores deserunt in voluptatibus aspernatur architecto
+            excepturi delectus soluta? Ipsa, deleniti dolorem hic consequatur
+            repellat eveniet quidem voluptate necessitatibus dolorum delectus
+            minus vitae, ut, veritatis sint ipsum magnam autem nam ex deserunt
+            debitis eaque ratione! Nobis, quidem assumenda.
+          </p>
+        </div>
+      </section>
+      <section className="ValuesBox">
+        <h2 className="mb-5 pb-2">OUR GOALS</h2>
         <div className="row align-items-center">
-          <div className="col-lg-6">
-            <img className="img-fluid d-block" src={aboutus} id="aboutUsPic" />
+          <div className="col-lg-4 col-md-6 py-3 mx-auto value">
+            <img className="img-fluid d-block mx-auto" src={aboutus} />
           </div>
-          <div className="col-lg-6 text-center my-5 py-3 my-lg-0">
-            <div className="row justify-content-center">
-              <div className="col-5 col-lg-9">
-                <h1 className="h2">OUR MISSION</h1>
-                <p className="aboutUsText my-4">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Neque, iste molestiae beatae, maiores deserunt in voluptatibus
-                  aspernatur architecto excepturi delectus soluta? Ipsa,
-                  deleniti dolorem hic consequatur repellat eveniet quidem
-                  voluptate necessitatibus dolorum delectus minus vitae, ut,
-                  veritatis sint ipsum magnam autem nam ex deserunt debitis
-                  eaque ratione! Nobis, quidem assumenda.Lorem ipsum dolor sit
-                  amet consectetur adipisicing elit. Neque, iste molestiae
-                  beatae, maiores deserunt in voluptatibus aspernatur architecto
-                  excepturi delectus soluta? Ipsa, deleniti dolorem hic
-                  consequatur repellat eveniet quidem voluptate necessitatibus
-                  dolorum delectus minus vitae, ut, veritatis sint ipsum magnam
-                  autem nam ex deserunt debitis eaque ratione! Nobis, quidem
-                  assumenda.
-                </p>
-              </div>
-            </div>
+          <div className="col-lg-4 col-md-6 py-3 value">
+            <img className="img-fluid d-block mx-auto" src={aboutus} />
+          </div>
+          <div className="col-lg-4 col-md-6 pt-3 mx-auto value">
+            <img className="img-fluid d-block mx-auto" src={aboutus} />
           </div>
         </div>
-      </div>
-      <div className="ValuesBox">
-        <h2>OUR GOALS</h2>
-        <section className="values py-5 mx-2">
-          <div className="row align-items-center">
-            <div className="col-lg-4">
-              <img
-                className="img-fluid d-block mx-auto"
-                src={aboutus}
-                id="ValuesPic"
-              />
-            </div>
-            <div className="col-lg-4">
-              <img
-                className="img-fluid d-block mx-auto"
-                src={aboutus}
-                id="ValuesPic"
-              />
-            </div>
-            <div className="col-lg-4">
-              <img
-                className="img-fluid d-block mx-auto"
-                src={aboutus}
-                id="ValuesPic"
-              />
-            </div>
-          </div>
-        </section>
-      </div>
+      </section>
       <Team />
     </>
   );
--- src/Pages/Team.tsx
+++ src/Pages/Team.tsx
@@ -2,93 +2,32 @@ import "./styles.css";
 import team from "./Pictures/logo.png";
 
 function Team() {
+  const teamMembers = [
+    { Name: "Hanko Ngu", Position: "Co-President" },
+    { Name: "Maria Kapitanenko", Position: "Co-President" },
+    { Name: "AbbyGale", Position: "VP of Marketing" },
+    { Name: "Jessica Singh", Position: "VP of Internal" },
+  ];
+
   return (
     <>
-      <section className="container-fluid Team">
+      <section className="container-fluid Team mt-5 pt-5">
         <h1>Meet Our Team</h1>
-        <section className="container-fluid mt-4 py-2">
+        <section className="container-fluid py-5">
           <div className="row align-items-center">
-            <div className="col-lg-3">
-              <img
-                className="img-fluid d-block mx-auto"
-                src={team}
-                id="teamphotos"
-              />
-              <div className="text-center teamTitles">
-                <h1>Hanko Ng</h1>
-                <h2>Co-President</h2>
+            {teamMembers.map((member) => (
+              <div key={member.Name} className="col-lg-3 col-md-4 col-6 mt-4">
+                <img
+                  className="img-fluid d-block mx-auto"
+                  src={team}
+                  id="teamphotos"
+                />
+                <div className="text-center teamMember mt-3">
+                  <h1>{member.Name}</h1>
+                  <h2>{member.Position}</h2>
+                </div>
               </div>
-            </div>
-            <div className="col-lg-3">
-              <img
-                className="img-fluid d-block mx-auto"
-                src={team}
-                id="teamphotos"
-              />
-              <div className="text-center teamTitles">
-                <h1>Maria Kapitanenko</h1>
-                <h2>Co-President</h2>
-              </div>
-            </div>
-            <div className="col-lg-3">
-              <img
-                className="img-fluid d-block mx-auto"
-                src={team}
-                id="teamphotos"
-              />
-              <div className="text-center teamTitles">
-                <h1>Sophie Bel</h1>
-                <h2>VP of Finance</h2>
-              </div>
-            </div>
-            <div className="col-lg-3">
-              <img
-                className="img-fluid d-block mx-auto"
-                src={team}
-                id="teamphotos"
-              />
-              <div className="text-center teamTitles">
-                <h1>Navraj Khattra</h1>
-                <h2>VP of Education</h2>
-              </div>
-            </div>
-          </div>
-        </section>
-        <section className="container-fluid pb-5">
-          <div className="row align-items-center">
-            <div className="col-lg-3">
-              <img
-                className="img-fluid d-block mx-auto"
-                src={team}
-                id="teamphotos"
-              />
-              <div className="text-center teamTitles">
-                <h1>Jessica Singh</h1>
-                <h2>VP of Internal</h2>
-              </div>
-            </div>
-            <div className="col-lg-3">
-              <img
-                className="img-fluid d-block mx-auto"
-                src={team}
-                id="teamphotos"
-              />
-              <div className="text-center teamTitles">
-                <h1>Mathews Jaison</h1>
-                <h2>VP of Innovations</h2>
-              </div>
-            </div>
-            <div className="col-lg-3 ">
-              <img
-                className="img-fluid d-block mx-auto"
-                src={team}
-                id="teamphotos"
-              />
-              <div className="text-center teamTitles">
-                <h1>Abbygale</h1>
-                <h2>VP of Maketing</h2>
-              </div>
-            </div>
+            ))}
           </div>
         </section>
       </section>
